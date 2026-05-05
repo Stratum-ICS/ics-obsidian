5ad8264b345c59ca5f284dfafc6777bad4123792
hahuy <quanghuyha098@gmail.com>
2026-05-05 16:32:20 +0700

    chore: scaffold obsidian plugin toolchain

---
+++ esbuild.config.mjs
@@ -0,0 +1,47 @@
+import esbuild from "esbuild";
+import process from "process";
+import builtins from "builtin-modules";
+
+const banner = `/*
+THIS IS A GENERATED FILE
+*/
+`;
+
+const isWatch = process.argv.includes("--watch");
+const prod = process.argv.includes("production");
+
+const context = await esbuild.context({
+  banner: { js: banner },
+  entryPoints: ["src/main.ts"],
+  bundle: true,
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+    ...builtins,
+  ],
+  format: "cjs",
+  target: "es2020",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "main.js",
+});
+
+if (isWatch) {
+  await context.watch();
+} else {
+  await context.rebuild();
+  await context.dispose();
+  process.exit(0);
+}
